368bc7b954298f243b2f41f1a3702efb073ec5b6
prestonhang <prestontanhang@gmail.com>
2024-05-15 01:53:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace college_course_tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
